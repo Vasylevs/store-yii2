832da390e875d5e7c8686641e472b0ce52eb6e32
Seliv <selivonenko007@gmail.com>
2018-06-17 14:49:15 +0300

    output popular products

---
+++ controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Seliv
+ * Date: 17.06.2018
+ * Time: 0:55
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class AppController extends Controller
+{
+
+}
--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -2,8 +2,14 @@
 
 namespace app\controllers;
 
+use app\models\Product;
+use Yii;
 
-class CategoryController
+class CategoryController extends AppController
 {
+    public function actionIndex(){
+        $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
 
+        return $this->render('index', compact('hits'));
+    }
 }
